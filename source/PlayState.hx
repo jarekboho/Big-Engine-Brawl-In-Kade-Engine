09ce988ccb4412947033f125b33afa1593bcd797
jarekboho <123318901+jarekboho@users.noreply.github.com>
2025-07-10 20:43:09 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -264,6 +264,7 @@ class PlayState extends MusicBeatState
 	public static var theFunne:Bool = true;
 
 	var inCutscene:Bool = false;
+	var usedTimeTravel:Bool = false;
 
 	public static var stageTesting:Bool = false;
 
@@ -2811,6 +2812,39 @@ if (isStoryMode)
 			}
 			#end
 		}
+
+		if (FlxG.keys.justPressed.TWO && songStarted)
+		{
+			if (!usedTimeTravel && Conductor.songPosition + 10000 < FlxG.sound.music.length)
+			{
+				usedTimeTravel = true;
+				FlxG.sound.music.pause();
+				vocals.pause();
+				Conductor.songPosition += 10000;
+				notes.forEachAlive(function(daNote:Note)
+				{
+					if (daNote.strumTime - 500 < Conductor.songPosition)
+					{
+						daNote.active = false;
+						daNote.visible = false;
+
+						daNote.kill();
+						notes.remove(daNote, true);
+						daNote.destroy();
+					}
+				});
+
+				FlxG.sound.music.time = Conductor.songPosition;
+				FlxG.sound.music.play();
+
+				vocals.time = Conductor.songPosition;
+				vocals.play();
+				new FlxTimer().start(0.5, function(tmr:FlxTimer)
+				{
+					usedTimeTravel = false;
+				});
+			}
+		}
 		#end
 
 		if (skipActive && Conductor.songPosition >= skipTo)
@@ -3133,6 +3167,8 @@ if (isStoryMode)
 
 		if (health <= 0 && !cannotDie)
 		{
+			if (!usedTimeTravel)
+			{
 				boyfriend.stunned = true;
 
 				persistentUpdate = false;
@@ -3165,6 +3201,9 @@ if (isStoryMode)
 					+ " | Misses: "
 					+ misses, iconRPC);
 				#end
+			}
+			else
+				health = 1;
 		}
 		if (!inCutscene && FlxG.save.data.resetButton && canReset)
 		{
